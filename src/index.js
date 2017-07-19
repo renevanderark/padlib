beb56b03aa0343a2d55193870ab23220d487b88e
rene <r.van.der.ark@gmail.com>
2017-07-19 16:33:49 +0200

    custom build target.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let controllers = {};
 const registerController = ({gamepad}) => {
   controllers[gamepad.index] = gamepad;
   knownMappings[gamepad.id] = knownMappings[gamepad.id] || {};
+  console.log(Object.keys(controllers), Object.keys(knownMappings));
 }
 
 const removeController = ({gamepad}) => {
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: "./build",
+    path: process.env.BUILD_TARGET || "./build",
     filename: "index.js"
   },
   plugins: [
